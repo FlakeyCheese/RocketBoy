2cfd554191920d67adc28e1eae6f36acd111af9d
771666 <771666@G107-6654>
2023-03-13 15:17:29 +0000

    Add project files.

---
+++ RocketBoy/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace RocketBoy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BackWall = new System.Windows.Forms.Label();
+            this.ScoreLbl = new System.Windows.Forms.Label();
+            this.TopWall = new System.Windows.Forms.Label();
+            this.BottomWall = new System.Windows.Forms.Label();
+            this.RocketBox = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.RocketBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BackWall
+            // 
+            this.BackWall.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackWall.Location = new System.Drawing.Point(-1, 0);
+            this.BackWall.Name = "BackWall";
+            this.BackWall.Size = new System.Drawing.Size(11, 464);
+            this.BackWall.TabIndex = 0;
+            // 
+            // ScoreLbl
+            // 
+            this.ScoreLbl.AutoSize = true;
+            this.ScoreLbl.Font = new System.Drawing.Font("GrilledCheese BTN", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLbl.Location = new System.Drawing.Point(38, 13);
+            this.ScoreLbl.Name = "ScoreLbl";
+            this.ScoreLbl.Size = new System.Drawing.Size(76, 33);
+            this.ScoreLbl.TabIndex = 1;
+            this.ScoreLbl.Text = "0 Km";
+            // 
+            // TopWall
+            // 
+            this.TopWall.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.TopWall.Location = new System.Drawing.Point(2, 0);
+            this.TopWall.Name = "TopWall";
+            this.TopWall.Size = new System.Drawing.Size(981, 11);
+            this.TopWall.TabIndex = 0;
+            // 
+            // BottomWall
+            // 
+            this.BottomWall.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BottomWall.Location = new System.Drawing.Point(2, 453);
+            this.BottomWall.Name = "BottomWall";
+            this.BottomWall.Size = new System.Drawing.Size(981, 11);
+            this.BottomWall.TabIndex = 0;
+            // 
+            // RocketBox
+            // 
+            this.RocketBox.Image = global::RocketBoy.Properties.Resources.rocket;
+            this.RocketBox.Location = new System.Drawing.Point(469, 187);
+            this.RocketBox.Name = "RocketBox";
+            this.RocketBox.Size = new System.Drawing.Size(101, 90);
+            this.RocketBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.RocketBox.TabIndex = 2;
+            this.RocketBox.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 50;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 457);
+            this.Controls.Add(this.RocketBox);
+            this.Controls.Add(this.ScoreLbl);
+            this.Controls.Add(this.BottomWall);
+            this.Controls.Add(this.TopWall);
+            this.Controls.Add(this.BackWall);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Rocket Boy";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.RocketBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label BackWall;
+        private System.Windows.Forms.Label ScoreLbl;
+        private System.Windows.Forms.Label TopWall;
+        private System.Windows.Forms.Label BottomWall;
+        private System.Windows.Forms.PictureBox RocketBox;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ RocketBoy/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RocketBoy
+{
+    public partial class Form1 : Form
+    {
+        Label[] rocks = new Label[20];
+        int rockCount = 0;
+        int score = 0;
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch ( e.KeyCode)
+                {
+                case Keys.Up:
+                    RocketBox.Top = RocketBox.Top - 5;
+                    break;
+                case Keys.Down:
+                    RocketBox.Top = RocketBox.Top + 5;
+                    break;
+                case Keys.Left:
+                    RocketBox.Left = RocketBox.Left - 5;
+                    break;
+                case Keys.Right:
+                    RocketBox.Left = RocketBox.Left + 5;
+                    break;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            int rockSize = rnd.Next(5, 30);            
+            score = score + 10;
+            ScoreLbl.Text = score.ToString() + "Km";
+            rocks[rockCount] = new Label()
+            {
+                Location = new Point(1000, rnd.Next(25,475)),                
+                AutoSize =false,
+                Size = new Size(rockSize, rockSize),
+                BackColor = Color.Crimson
+             };
+            this.Controls.Add(rocks[rockCount]);
+            timer2.Enabled = true;
+            rockCount++;
+            if (rockCount == 20)
+            { rockCount = 0; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            for (int n = 0; n < 20; n++)
+            {
+                if (rocks[n] is null)
+                { continue; }
+                rocks[n].Left = rocks[n].Left - 5;
+                
+                if (rocks[n].Bounds.IntersectsWith(RocketBox.Bounds))
+                {
+                    RocketBox.Image = RocketBoy.Properties.Resources.boom;
+                    timer1.Enabled = false;
+                    timer2.Enabled = false;
+                }
+                if (rocks[n].Bounds.IntersectsWith(BackWall.Bounds))
+                {
+                    this.Controls.Remove(rocks[n]);
+                    rocks[n] = null;
+                }
+            }
+        }
+    }
+}
